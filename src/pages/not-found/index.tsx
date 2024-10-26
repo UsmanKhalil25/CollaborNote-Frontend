dca0380487d5c9ffbe30950333152511cfc1563a
UsmanKhalil25 <usmankhalil8011@gmail.com>
2024-10-26 16:48:51 +0500

    Refactor to use previous components

--- src/pages/not-found/index.tsx
+++ src/pages/not-found/index.tsx
@@ -1,18 +1,24 @@
-import { Link } from 'react-router-dom';
+import { Button } from "@/components/ui/button";
+import { useNavigate } from "react-router-dom";
 
 export default function NotFoundPage() {
+  const navigate = useNavigate();
+
+  const handleHomeNavigation = () => {
+    navigate("/");
+  };
+
   return (
     <div className="flex flex-col justify-center items-center text-center h-screen bg-black">
-      <h1 className="text-8xl font-bold text-white transform transition duration-300 hover:scale-110">404</h1>
+      <h1 className="text-8xl font-bold text-white transform transition duration-300 hover:scale-110">
+        404
+      </h1>
       <p className="text-lg text-gray-500 mt-4">
         Sorry, the page you&apos;re looking for doesn&apos;t exist.
       </p>
-      <Link
-        to="/"
-        className="mt-6 px-6 py-2 bg-white text-black font-medium rounded-md shadow hover:bg-gray-800 transition"
-      >
+      <Button className="mt-4" onClick={handleHomeNavigation}>
         Return to website
-      </Link>
+      </Button>
     </div>
   );
 }
