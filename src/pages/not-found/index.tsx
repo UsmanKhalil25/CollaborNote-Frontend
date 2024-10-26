42e6226dee91cb951f456dc85f0506906a68666c
TahnanAmir <m.tahnanamir@gmail.com>
2024-10-26 16:34:59 +0500

    Functionality for displaying Not Found page

--- src/pages/not-found/index.tsx
+++ src/pages/not-found/index.tsx
@@ -1,8 +1,18 @@
+import { Link } from 'react-router-dom';
+
 export default function NotFoundPage() {
   return (
-    <div className="flex justify-center items-center text-white h-full">
-      <h1>404 - Page Not Found</h1>
-      <p>Sorry, the page you&apos;re looking for doesn&apos;t exist.</p>
+    <div className="flex flex-col justify-center items-center text-center h-screen bg-black">
+      <h1 className="text-8xl font-bold text-white transform transition duration-300 hover:scale-110">404</h1>
+      <p className="text-lg text-gray-500 mt-4">
+        Sorry, the page you&apos;re looking for doesn&apos;t exist.
+      </p>
+      <Link
+        to="/"
+        className="mt-6 px-6 py-2 bg-white text-black font-medium rounded-md shadow hover:bg-gray-800 transition"
+      >
+        Return to website
+      </Link>
     </div>
   );
 }
